0ed69e4010b42ed5ec89b53ae7bfc82b4263f25e
dev-cynic <htetaunglynn@my.uopeople.edu>
2025-12-15 23:02:48 -0500

    Completed web step definitions: button click, message verification, results checking

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -98,19 +98,40 @@ def step_impl(context, element_name):
 
 ##################################################################
 # This code works because of the following naming convention:
-# The buttons have an id in the html hat is the button text
-# in lowercase followed by '-btn' so the Clean button has an id of
+# The buttons have an id in the html that is the button text
+# in lowercase followed by '-btn' so the Clear button has an id of
 # id='clear-btn'. That allows us to lowercase the name and add '-btn'
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower().replace(' ', '-') + '-btn'
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.presence_of_element_located((By.ID, button_id))
+    )
+    element.click()
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    element = context.driver.find_element(By.ID, 'flash_message')
+    assert(message in element.text)
+
+@then('I should see "{text_string}" in the results')
+def step_impl(context, text_string):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(text_string in element.text)
+
+@then('I should not see "{text_string}" in the results')
+def step_impl(context, text_string):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert(text_string not in element.text)
 
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
-# prefixed by ID_PREFIX so the Name field has an id='pet_name'
-# We can then lowercase the name and prefix with pet_ to get the id
+# prefixed by ID_PREFIX so the Name field has an id='product_name'
+# We can then lowercase the name and prefix with product_ to get the id
 ##################################################################
 
 @then('I should see "{text_string}" in the "{element_name}" field')
